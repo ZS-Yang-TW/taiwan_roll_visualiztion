f1b1960cf63f91009b5fb0b64b20eb920b881948
steven <msycomtw@gmail.com>
2024-03-04 20:42:32 +0800

    opt: optimized project code

--- project.py
+++ project.py
@@ -1,41 +1,43 @@
-from xml.etree import ElementTree as ET
 from PIL import ImageColor
 import json
+import xml.etree.ElementTree as ET
 
 # 讀取投票結果資料
 with open("voting_results.json", "r") as file:
     voting_results = json.load(file)
 
-# 初始化新的字典用於儲存計算结果 
-percentages = {}
-winners = {}
-votes_difference = {}
-colors = {}
-
-max_votes_difference = 0
-
-# 以縣市為迭代單位，計算得票率、獲勝者、得票差距
-for city, total_votes in voting_results["district_total"].items():
-    votes = {party: voting_results[party][city] for party in ["dpp", "kmt", "tpp"]}
-    percentages[city] = {party: votes[party] / total_votes * 100 for party in votes}  # 計算縣市的得票率
-    sorted_parties = sorted(percentages[city].items(), key=lambda x: x[1], reverse=True)  # 該縣市的得票排序
-
-    # 計算縣市第一名和第二名的得票差距
-    votes_difference[city] = sorted_parties[0][1] - sorted_parties[1][1]
-    max_votes_difference = max(max_votes_difference, votes_difference[city])
-
-    # 計算縣市的顏色
-    winner_party = sorted_parties[0][0]  # 縣市獲勝的黨派
-    ratio = votes_difference[city] / max_votes_difference
+# 以各縣市為迭代單位
+votes = {}
+parties = {"dpp", "kmt", "tpp"}
+for city in voting_results["district_total"]:
+    party_votes = {party: voting_results[party][city] for party in parties}
+    votes.update({city: party_votes}) # 各縣市、各黨候選人的得票數
     
+percentage = {}
+for city in voting_results["district_total"]:
+    total_votes = voting_results["district_total"][city]
+    party_percentage = {party: votes[city][party] / total_votes * 100 for party in votes[city]} # 各縣市、各黨候選人的得票率
+    percentage.update({city: party_percentage})
+
+# 以縣市為迭代單位，找到獲勝者、計算得票差距
+max_votes_difference = 0
+colors = {}
+for city in voting_results["district_total"]:
+    sorted_parties = sorted(percentage[city].items(), key=lambda x: x[1], reverse=True)  # 該縣市的得票排序
+    winner = sorted_parties[0][0]  # 縣市獲勝的黨派
+    votes_difference = sorted_parties[0][1] - sorted_parties[1][1]  # 計算縣市第一名和第二名的得票差距
+    max_votes_difference = max(votes_difference, max_votes_difference)  # 找到最大的得票差距
+    
+    # 計算縣市的顏色
+    ratio = votes_difference / max_votes_difference
     hsl_values = {
         "tpp": (177, 61, int(75 - 40 * ratio)),
         "dpp": (130, 60, int(75 - 40 * ratio)),
         "kmt": (212, 100, int(85 - 40 * ratio)),
     }
-    
+
     # 根據獲勝者的黨派，計算 HSL 值 
-    hsl = f"hsl({hsl_values[winner_party][0]}, {hsl_values[winner_party][1]}%, {hsl_values[winner_party][2]}%)"
+    hsl = f"hsl({hsl_values[winner][0]}, {hsl_values[winner][1]}%, {hsl_values[winner][2]}%)"
     rgb_hex = ImageColor.getrgb(hsl)
     colors[city] = f"#{rgb_hex[0]:02x}{rgb_hex[1]:02x}{rgb_hex[2]:02x}"
 
@@ -53,7 +55,6 @@ for path in svg_tree.iterfind(".//{http://www.w3.org/2000/svg}path"):
 
     if title is not None:
         target = title.text.strip().split(" ")[0]
-        # print(target)
         path.set("fill", colors[target])
 
 # 如果成功修改，則保存修改後的SVG內容到一個新檔案
